cd0b78ca4b1e797d859b72db22033b459cc1a5e3
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-05-02 08:38:46 -0400

    starting traders

---
+++ NPC.py
@@ -0,0 +1,13 @@
+class npc():
+    def __init__(self, name):
+        self.name = name
+
+class seller(npc):
+    def __init__(self, name, trading, products):
+        super().__init__(name)
+        self.trading = trading
+        self.products = products
+    def __str__(self):
+        return f"{self.name}"
+
+Glove_seller = seller('David Gloveman','')
