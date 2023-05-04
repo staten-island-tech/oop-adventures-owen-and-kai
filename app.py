5360f2c0e076968f582d9aca64bcc5a09dbe7919
OwenZhao2 <124058634+OwenZhao2@users.noreply.github.com>
2023-05-03 20:37:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,3 +31,12 @@ while True:
         print("That's not a choice kid... ")
         main_menu = input("Main Menu  Choices: Play/Credits/Tutorial ")
         continue
+
+player_customization_screen = input("Welcome to the player customization screen, here you will make your own custom player that will progress through the story. Please refrain from using any personal information as we will not take any responsibility if your information gets leaked, thank you.  Choices: Continue ")
+
+while True:
+    if player_customization_screen == "Continue":
+        player_username = input("The next King/Queen of Boxing will be: ")
+        player_age = input("What is your age? ")
+        player_gender = input("What is your gender? ")
+        backstory_skip = input("Would you like to skip the backstory? Y/N ")
