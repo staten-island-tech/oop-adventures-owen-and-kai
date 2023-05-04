dbb539a5f01667fd1c1c99c957c3a3a0a9589e75
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-05-04 08:29:57 -0400

    tutorial boos done

--- NPC.py
+++ NPC.py
@@ -5,6 +5,14 @@ class npc():
 class enemy(npc):
     def __init__(self, name, health, attacks, stamina, technique):
         super().__init__(name)
+        self.health = health
+        self.attacks = attacks 
+        self.samina = stamina 
+        self.technique = technique
+    def __str__(self):
+        return f"{self.name},{self.health},{ self.attacks},{self.stamina},{self.technique}"
+
+Tutorial_Boss = enemy('John Parkinson','500','10','100','5') 
 
 class seller(npc):
     def __init__(self, name, trading, products):
@@ -12,6 +20,6 @@ class seller(npc):
         self.trading = trading
         self.products = products
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name},{self.trading},{self.products}"
 
 Glove_seller = seller('David Gloveman','')
