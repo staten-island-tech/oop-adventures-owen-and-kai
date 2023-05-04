e1be380acff4e6a7150a49c609e0d0351c315dbd
OwenZhao2 <124058634+OwenZhao2@users.noreply.github.com>
2023-05-04 08:30:32 -0400

    5/4/23

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ while True:
         else:
             print("That's not a choice kid... ")
             credits = input("Credits  Choices: Back ")
-        continue
+            continue
         
     if main_menu == "Tutorial":
         tutorial = input("Tutorial  Choices: Back ")
@@ -21,7 +21,7 @@ while True:
         else:
             print("That's not a choice kid... ")
             tutorial = input("Tutorial  Choices: Back ")
-        continue
+            continue
 
     if main_menu == "Play":
         print("Here we go! ")
@@ -30,13 +30,38 @@ while True:
     else:
         print("That's not a choice kid... ")
         main_menu = input("Main Menu  Choices: Play/Credits/Tutorial ")
-    continue
+        continue
 
 player_customization_screen = input("Welcome to the player customization screen, here you will make your own custom player that will progress through the story. Please refrain from using any personal information as we will not take any responsibility if your information gets leaked, thank you.  Choices: Continue ")
 
 while True:
     if player_customization_screen == "Continue":
         player_username = input("The next King/Queen of Boxing will be: ")
-        player_age = input("What is your age? ")
+        player_story_name = player_username
+        player_age = int(input("What is your age? "))
+        player_story_age = player_age
         player_gender = input("What is your gender? ")
-        backstory_skip = input("Would you like to skip the backstory? Y/N ")
+        if player_gender == "M":
+            player_pronouns = "He"
+            break
+
+        if player_gender == "F":
+            player_pronouns = "She"
+            break
+
+        else:
+            print("That's not a gender kid... ")
+            player_gender = input("What is your gender? ")
+            continue
+
+backstory_skip = input("Would you like to skip the backstory? Y/N")
+while True:
+    if backstory_skip == "Y":
+        break
+
+    if backstory_skip == "N":
+        backstory = input()
+
+    else:
+        print("That's not a choice kid... ")
+        continue
