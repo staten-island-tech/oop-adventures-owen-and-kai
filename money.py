04cf955be69088558594c8638818110962b19d8c
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-05-17 08:40:21 -0400

    money 2

--- money.py
+++ money.py
@@ -1,7 +1,13 @@
-##currency = 0
-##currency = float(currency) + 100
-d = {'money':100}
-def buyBread():
-  d[money]=d[money]-10
+if location_choice == glove_shop:
+   input ('Hello welcome to my shop. We can sell you any gloves or something a little special if you catch my drift...  Choices: Buy Gloves/Buy Steroids' )
+   if input == 'Buy Gloves':
+    input ('Here are the glove options: Light Gloves, Heavy Gloves, Fiery Gloves, Fingerless Gloves, Bell Gloves, Latex Gloves, Holy Gloves, Police Gloves, Lufffy\'s Gloves, Only Finger Gloves')
+
+currency = 0
+currency = float(currency) + 100
+currency = float(currency) + 200
+currency = float(currency) - 50
+print (currency)
+
 
 
