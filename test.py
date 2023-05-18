55821a0819928f39665889eb63b4f69a8fe9aa3c
OwenZhao2 <124058634+OwenZhao2@users.noreply.github.com>
2023-05-18 08:40:58 -0400

    5/18/23 Much Organization to do

---
+++ test.py
@@ -0,0 +1,20 @@
+
+user_defend()
+user_attack()
+ai_defend()
+ai_attack()
+
+while true:
+     if player_turn == "Defend":
+        ask
+        user_defend(ask, enemy_turn)
+class combat:
+    def user_defend
+def user_defend(ask, enemy_turn):
+    if enemy_turn == attack:
+
+tutorial = combat(user, tutorial_boss):
+while true:
+    combat.player_turn
+    combat.ai_turn
+
