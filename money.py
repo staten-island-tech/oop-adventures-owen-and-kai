5bdf8246383fca4285b3a1c8cc1a5773ad1f00f7
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-05-22 08:40:18 -0400

    inventory

--- money.py
+++ money.py
@@ -1,11 +1,15 @@
-inventory = []
+def addtoinventory(Item):
+   inventory.append(Item)
+
+inventory = ['Basic_Gloves']
 if location_choice == glove_shop:
    if location_choice == input ('Hello welcome to my shop. We can sell you any gloves or something a little special if you catch my drift...  Choices: Buy Gloves/Buy Steroids' ):
-    if input == 'Buy Gloves':
+    if purchase == input('Buy Gloves'):
         glove_choices ==input('Here are the glove options: Light Gloves, Heavy Gloves, Fiery Gloves, Fingerless Gloves, Bell Gloves, Latex Gloves, Holy Gloves, Police Gloves, Only Finger Gloves')
     if input == 'Light GLoves':
         glove_purchase == input ('The light gloves will increase your dexterity and decrease stamina use. Would you like to purchase it? Choices: Y/N')
         if input == 'Y':
+           addtoinventory('BasicGloves')
            
            
 
@@ -15,3 +19,4 @@ currency = float(currency) + 100
 currency = float(currency) + 200
 currency = float(currency) - 50
 print (currency)
+
