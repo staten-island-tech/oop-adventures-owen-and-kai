9c88809d9fe0853bb487a9b4b32c84647a8dcbb0
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-05-23 08:38:04 -0400

    some progress

--- money.py
+++ money.py
@@ -6,10 +6,14 @@ if location_choice == glove_shop:
    if location_choice == input ('Hello welcome to my shop. We can sell you any gloves or something a little special if you catch my drift...  Choices: Buy Gloves/Buy Steroids' ):
     if purchase == input('Buy Gloves'):
         glove_choices ==input('Here are the glove options: Light Gloves, Heavy Gloves, Fiery Gloves, Fingerless Gloves, Bell Gloves, Latex Gloves, Holy Gloves, Police Gloves, Only Finger Gloves')
-    if input == 'Light GLoves':
-        glove_purchase == input ('The light gloves will increase your dexterity and decrease stamina use. Would you like to purchase it? Choices: Y/N')
-        if input == 'Y':
-           addtoinventory('BasicGloves')
+        if input == 'Light GLoves':
+            glove_purchase == input ('The light gloves will increase your dexterity and decrease stamina use. Would you like to purchase it? Choices: Y/N')
+            if input == 'Y':
+                addtoinventory('Light Gloves')
+            if input == "N":
+               print
+        if input == 'Heavy Gloves':
+            glove_purchase == input('The Heavy Gloves Will do more damage and give you more defense but will use more stamina. Would you like to purchase it? Choices: Y/N')
            
            
 
