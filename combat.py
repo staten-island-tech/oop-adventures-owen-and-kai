35465c4fb8c50649f908b5f23f81e5f15ff24476
OwenZhao2 <124058634+OwenZhao2@users.noreply.github.com>
2023-05-30 08:34:17 -0400

    Something good is happening

--- combat.py
+++ combat.py
@@ -5,37 +5,35 @@ from defense import Block_Forward, Block_Side, Block_Down
 from resting import Relax_Muscles, Put_Guard_Down, Full_Body_Relaxation
 from NPC import Tutorial_Boss, Round_1_Boss, Round_2_Boss, Round_3_Boss, Quarter_Finals_Boss, Semi_Finals_Boss, Final_Round_Boss, Glove_seller, Black_Market_Dealer, Coach, The_Miracle_Builder
 
-stat_point = None
-Attack = 1
-Defend = 2
-Rest = 3
-Hit = 4
-Miss = 5
+def combat_system(player_health, player_stamina, player_story_name):
+    stat_point = None
+    Attack = 1
+    Defend = 2
+    Rest = 3
+    Hit = 4
+    Miss = 5
 
-player_dexterity_points = []
-player_strength_points = []
-player_stamina_points = []
-player_technique_points = []
+    player_dexterity_points = []
+    player_strength_points = []
+    player_stamina_points = []
+    player_technique_points = []
 
-player_health = 500 + len(player_dexterity_points) * 10
-player_strength = 10 + len(player_strength_points) * 1
-player_stamina = 100 + len(player_stamina_points) * 5
-player_technique = 5 + len(player_technique_points) * 1
+    player_health = 500 + len(player_dexterity_points) * 10
+    player_strength = 10 + len(player_strength_points) * 1
+    player_stamina = 100 + len(player_stamina_points) * 5
+    player_technique = 5 + len(player_technique_points) * 1
 
-tutorial_boss_enemy_actions = [Attack, Attack, Attack, Defend]
-tutorial_boss_enemy_turn = random.choice(tutorial_boss_enemy_actions)
+    tutorial_boss_enemy_actions = [Attack, Attack, Attack, Defend]
+    tutorial_boss_enemy_turn = random.choice(tutorial_boss_enemy_actions)
 
-if tutorial_boss_enemy_turn == Attack:
     tutorial_boss_enemy_spontaneous_strike = [Hit, Miss, Miss, Miss]
     tutorial_boss_enemy_spontaneous_strike_chance = random.choice(tutorial_boss_enemy_spontaneous_strike)
 
-tutorial_boss_enemy_attack = [Jab, Straight, Left_Hook, Right_Hook, Right_Uppercut]
-tutorial_boss_turn_attack = random.choice(tutorial_boss_enemy_attack)
+    tutorial_boss_enemy_attack = [Jab, Straight, Left_Hook, Right_Hook, Right_Uppercut]
+    tutorial_boss_turn_attack = random.choice(tutorial_boss_enemy_attack)
 
-player_spontaneous_strike = [Hit, Miss, Miss, Miss]
-player_spontaneous_strike_chance = random.choice(player_spontaneous_strike)
-
-def combat_system(player_health, player_stamina, player_story_name):
+    player_spontaneous_strike = [Hit, Miss, Miss, Miss]
+    player_spontaneous_strike_chance = random.choice(player_spontaneous_strike)
     while player_health and Tutorial_Boss.health != 0:
         player_turn = input("What do you want to do?  Choices: 1. Attack/2. Defend/3. Rest ")
         if player_turn == "1":
