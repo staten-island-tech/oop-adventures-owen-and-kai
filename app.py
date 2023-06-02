518dbcd13481d80b7cc7665346aecb790fc1c613
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-06-02 08:27:26 -0400

    hi

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from defense import Block_Forward, Block_Side, Block_Down
 from resting import Relax_Muscles, Put_Guard_Down, Full_Body_Relaxation
 from NPC import Tutorial_Boss, Round_1_Boss, Round_2_Boss, Round_3_Boss, Quarter_Finals_Boss, Semi_Finals_Boss, Final_Round_Boss, Glove_seller, Black_Market_Dealer, Coach, The_Miracle_Builder
 from combat import combat_system
+print(combat_system)
 
 stat_point = None
 Attack = 1
